28d43b6b0895077c8be8b0a352a3a35bbbd12216
Alex Xie <111574544+amiyuki7@users.noreply.github.com>
2024-08-29 09:01:03 +1000

    Use conditional composer to render widget tree based on `AuthState`

--- lib/composition_root.dart
+++ lib/composition_root.dart
@@ -2,33 +2,54 @@ import 'package:backend/backend.dart';
 import 'package:flutter/material.dart';
 import 'package:rethink_db_ns/rethink_db_ns.dart';
 import 'package:zonin/screens/login_screen.dart';
+import 'package:zonin/screens/main_screen.dart';
 import 'package:zonin/state/auth/auth_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:zonin/theme.dart';
 
 class CompositionRoot {
   static late RethinkDb _r;
   static late Connection _conn;
   static late IUserService _userService;
-  static late AuthCubit authCubit;
+  // static late AuthCubit authCubit;
 
   static Future<void> configure() async {
     _r = RethinkDb();
     _conn = await _r.connect(host: '127.0.0.1', port: 28015);
     _userService = UserService(_r, _conn);
-    authCubit = AuthCubit(_userService);
+    // authCubit = AuthCubit(_userService);
   }
 
   static Widget composeLoginUI() {
-    //   return BlocProvider<AuthCubit>(
-    //     create: (BuildContext context) => authCubit,
-    //     child: LoginScreen(),
-    //   );
-    // }
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (BuildContext context) => authCubit),
-      ],
-      child: const LoginScreen(),
+    // return MultiBlocProvider(
+    //   providers: [
+    //     BlocProvider(create: (BuildContext context) => authCubit),
+    //   ],
+    //   child: const LoginScreen(),
+    // );
+    return const LoginScreen();
+  }
+}
+
+class Composer extends StatelessWidget {
+  const Composer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider<AuthCubit>(
+      create: (context) => AuthCubit(CompositionRoot._userService),
+      child: MaterialApp(
+        title: 'Zonin',
+        debugShowCheckedModeBanner: false,
+        theme: darkTheme(context),
+        home: BlocBuilder<AuthCubit, AuthState>(
+          builder: (context, state) {
+            return (state is Authenticated)
+                ? MainScreen(state.user)
+                : CompositionRoot.composeLoginUI();
+          },
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:zonin/composition_root.dart';
-import 'package:zonin/screens/login_screen.dart';
-import 'package:zonin/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await CompositionRoot.configure();
 
-  runApp(const MyApp());
+  // runApp(const MyApp());
+  runApp(const Composer());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Zonin',
-      debugShowCheckedModeBanner: false,
-      theme: darkTheme(context),
-      home: CompositionRoot.composeLoginUI(),
-    );
-  }
-}
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Zonin',
+//       debugShowCheckedModeBanner: false,
+//       theme: darkTheme(context),
+//       // home: CompositionRoot.composeLoginUI(),
+//       home: Composer(),
+//     );
+//   }
+// }
