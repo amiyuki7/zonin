c6d7b694473998465471da3d16a59b2118438d8b
Alex Xie <111574544+amiyuki7@users.noreply.github.com>
2024-08-29 11:32:14 +1000

    Update setup script

--- setup_rethinkdb.py
+++ setup_rethinkdb.py
@@ -7,6 +7,13 @@ conn = r.connect(db='test')
 r.table_create('users').run(conn)
 r.table('users').index_create('email').run(conn)
 
+r.table_create('activities').run(conn)
+
 # Create a default user (for testing purposes)
 hash = hashlib.sha256(b"admin888").hexdigest()
-r.table('users').insert({'email': 'admin@zonin.dev', 'password': hash}).run(conn)
+result = r.table('users').insert({'email': 'admin@zonin.dev', 'password': hash}, return_changes=True).run(conn)
+userId = result['changes'][0]['new_val']['id']
+
+# Create default activities for our default user, 'style': ' '
+r.table('activities').insert({'userId': userId, 'name': 'Mathematics', 'description': 'Extension 2', 'style': ' ', 'order': 0}).run(conn)
+r.table('activities').insert({'userId': userId, 'name': 'English', 'description': ' ', 'style': ' ', 'order': 1}).run(conn)
